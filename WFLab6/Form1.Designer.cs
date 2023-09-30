27225f165dd6a93d78d0a3f1d0817599dd1af7cb
Hiếu Hoàng Văn <hieuhv@uef.edu.vn>
2023-09-30 18:13:50 +0700

    to do  task lab 5

--- WFLab6/Form1.Designer.cs
+++ WFLab6/Form1.Designer.cs
@@ -28,111 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.checkBox5 = new System.Windows.Forms.CheckBox();
-            this.checkBox6 = new System.Windows.Forms.CheckBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lvStaff = new System.Windows.Forms.ListView();
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDBO = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colPhone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colAddress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.dtpkDBO = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // groupBox1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(81, 81);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.btnUpdate);
+            this.groupBox1.Controls.Add(this.btnDel);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dtpkDBO);
+            this.groupBox1.Controls.Add(this.txtAddress);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Location = new System.Drawing.Point(85, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(545, 263);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
             // 
-            // checkBox2
+            // groupBox2
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(81, 104);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = "checkBox2";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.groupBox2.Controls.Add(this.lvStaff);
+            this.groupBox2.Location = new System.Drawing.Point(0, 292);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(675, 263);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "List";
             // 
-            // button1
+            // lvStaff
             // 
-            this.button1.Location = new System.Drawing.Point(81, 225);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.lvStaff.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colName,
+            this.colDBO,
+            this.colPhone,
+            this.colAddress});
+            this.lvStaff.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvStaff.FullRowSelect = true;
+            this.lvStaff.GridLines = true;
+            this.lvStaff.HideSelection = false;
+            this.lvStaff.Location = new System.Drawing.Point(3, 16);
+            this.lvStaff.Name = "lvStaff";
+            this.lvStaff.Size = new System.Drawing.Size(669, 244);
+            this.lvStaff.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.lvStaff.TabIndex = 0;
+            this.lvStaff.UseCompatibleStateImageBehavior = false;
+            this.lvStaff.View = System.Windows.Forms.View.Details;
+            this.lvStaff.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lvStaff_ItemSelectionChanged);
             // 
-            // checkBox3
+            // colName
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(81, 129);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(80, 17);
-            this.checkBox3.TabIndex = 0;
-            this.checkBox3.Text = "checkBox1";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.colName.Text = "Name";
+            this.colName.Width = 165;
             // 
-            // checkBox4
+            // colDBO
             // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(81, 152);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(80, 17);
-            this.checkBox4.TabIndex = 1;
-            this.checkBox4.Text = "checkBox2";
-            this.checkBox4.UseVisualStyleBackColor = true;
+            this.colDBO.Text = "DBO";
+            this.colDBO.Width = 109;
             // 
-            // checkBox5
+            // colPhone
             // 
-            this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(81, 175);
-            this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(80, 17);
-            this.checkBox5.TabIndex = 0;
-            this.checkBox5.Text = "checkBox1";
-            this.checkBox5.UseVisualStyleBackColor = true;
+            this.colPhone.Text = "Phone";
+            this.colPhone.Width = 151;
             // 
-            // checkBox6
+            // colAddress
             // 
-            this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(81, 198);
-            this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(80, 17);
-            this.checkBox6.TabIndex = 1;
-            this.checkBox6.Text = "checkBox2";
-            this.checkBox6.UseVisualStyleBackColor = true;
+            this.colAddress.Text = "Address";
+            this.colAddress.Width = 237;
             // 
-            // button2
+            // txtName
             // 
-            this.button2.Location = new System.Drawing.Point(81, 267);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(89, 45);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(186, 26);
+            this.txtName.TabIndex = 0;
             // 
-            // button3
+            // txtPhone
             // 
-            this.button3.Location = new System.Drawing.Point(81, 313);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button1";
-            this.button3.UseVisualStyleBackColor = true;
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(353, 45);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(186, 26);
+            this.txtPhone.TabIndex = 0;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(353, 89);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(186, 26);
+            this.txtAddress.TabIndex = 0;
+            // 
+            // dtpkDBO
+            // 
+            this.dtpkDBO.CustomFormat = "dd/MM/yyyy";
+            this.dtpkDBO.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpkDBO.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpkDBO.Location = new System.Drawing.Point(89, 89);
+            this.dtpkDBO.Name = "dtpkDBO";
+            this.dtpkDBO.Size = new System.Drawing.Size(186, 26);
+            this.dtpkDBO.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DBO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(301, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Address";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(89, 157);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(110, 30);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDel.Location = new System.Drawing.Point(205, 157);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(110, 30);
+            this.btnDel.TabIndex = 3;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(319, 157);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(110, 30);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(81, 354);
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(435, 157);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 2;
+            this.button4.Size = new System.Drawing.Size(110, 30);
+            this.button4.TabIndex = 3;
             this.button4.Text = "button1";
             this.button4.UseVisualStyleBackColor = true;
             // 
@@ -140,36 +239,39 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.checkBox6);
-            this.Controls.Add(this.checkBox5);
-            this.Controls.Add(this.checkBox4);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(732, 589);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
-        private System.Windows.Forms.CheckBox checkBox5;
-        private System.Windows.Forms.CheckBox checkBox6;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpkDBO;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView lvStaff;
+        private System.Windows.Forms.ColumnHeader colName;
+        private System.Windows.Forms.ColumnHeader colDBO;
+        private System.Windows.Forms.ColumnHeader colPhone;
+        private System.Windows.Forms.ColumnHeader colAddress;
     }
 }
 
--- WFLab6/Form1.cs
+++ WFLab6/Form1.cs
@@ -16,5 +16,92 @@ namespace WFLab6
         {
             InitializeComponent();
         }
+        public void ResetInput()
+        {
+            txtAddress.Text = txtName.Text = txtPhone.Text = "";
+            dtpkDBO.Value = DateTime.Now;
+        }
+        public bool CheckInput()
+        {
+
+            if(txtName.Text.Trim().Length <5 || txtName.Text.Trim().Length >36)
+            {
+                MessageBox.Show("Length is Val!", "Error input", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                txtName.Text = "";
+                txtName.Focus();
+                return false;
+            }
+
+            return true;
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(CheckInput() != true)
+                {
+                    return;
+                }
+
+                ListViewItem item = new System.Windows.Forms.ListViewItem(new string[] { txtName.Text, dtpkDBO.Text, txtPhone.Text, txtAddress.Text });
+                lvStaff.Items.Add(item);
+                ResetInput();
+                txtName.Focus();
+            }
+            catch
+            {
+                //log error;
+            }
+            
+        }
+
+        private void lvStaff_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            txtName.Enabled = false;
+            try
+            {
+                ListViewItem item;
+                item = e.Item;
+
+                txtName.Text = item.Text;
+                dtpkDBO.Text = item.SubItems[1].Text;
+                txtPhone.Text = item.SubItems[2].Text;
+                txtAddress.Text = item.SubItems[3].Text;
+            }
+            catch { }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int total = lvStaff.Items.Count;
+            for(int i=0; i<total; i++)
+            {
+                if(lvStaff.Items[i].Text == txtName.Text)
+                {
+                    lvStaff.Items[i].SubItems[1].Text = dtpkDBO.Text;
+                    lvStaff.Items[i].SubItems[2].Text = txtPhone.Text;
+                    lvStaff.Items[i].SubItems[3].Text = txtAddress.Text;
+                    ResetInput();
+                    return;
+                }
+            }
+            
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            int total = lvStaff.Items.Count;
+            for (int i = 0; i < total; i++)
+            {
+                if (lvStaff.Items[i].Text == txtName.Text)
+                {
+                    lvStaff.Items.RemoveAt(i);
+                    ResetInput();
+                    return;
+                }
+            }
+        }
     }
 }
